4d2fcf669130e64a0fce65cb6d26b841503984e1
grandmind <darichey1@gmail.com>
2016-08-12 18:32:01 -0500

    Added proper token getting to tests.

--- src/test/java/com/darichey/discord/Main.java
+++ src/test/java/com/darichey/discord/Main.java
@@ -4,14 +4,21 @@ import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 	static IDiscordClient client;
+	static String TOKEN = "";
 
 	public static void main(String[] args) {
-		try {
-			client = new ClientBuilder().withToken("this didn't happen").login();
+		try (BufferedReader reader = new BufferedReader(new FileReader("token.txt"))){
+			TOKEN = reader.readLine();
+			client = new ClientBuilder().withToken(TOKEN).login();
 			CommandRegistry.getRegistryForClient(client).register(new CommandHandler());
-		} catch (DiscordException e) {
+		} catch (IOException | DiscordException e) {
 			e.printStackTrace();
 		}
 	}
