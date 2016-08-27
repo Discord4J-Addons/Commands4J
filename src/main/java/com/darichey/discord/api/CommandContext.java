7a232fa77d00485111e957bdfdea5d40029da7a2
poncethecat <thecatponce@gmail.com>
2016-08-27 12:18:40 -0400

    Added the ability for args to be separated by spaces or quotes.
    The above says it all.

--- src/main/java/com/darichey/discord/api/CommandContext.java
+++ src/main/java/com/darichey/discord/api/CommandContext.java
@@ -2,6 +2,11 @@ package com.darichey.discord.api;
 
 import sx.blah.discord.handle.obj.IMessage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CommandContext {
 
 	private final IMessage message;
@@ -11,7 +16,14 @@ public class CommandContext {
 	public CommandContext(IMessage message) {
 		this.message = message;
 		this.name = message.getContent().substring(1).substring(0, message.getContent().contains(" ") ? message.getContent().indexOf(" ") + 1 : message.getContent().length() - 1);
-		this.args = (message.getContent().substring(message.getContent().contains(" ") ? message.getContent().indexOf(" ") + 1 : message.getContent().length())).split("\\s+");
+		//this.args = (message.getContent().substring(message.getContent().contains(" ") ? message.getContent().indexOf(" ") + 1 : message.getContent().length())).split("\\s+");
+		List<String> list = new ArrayList<String>();
+		Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(message.getContent().substring(1).replace(name, ""));
+		while (m.find()) {
+			list.add(m.group(1));
+		}
+		String testargs[] = new String[list.size()];
+		this.args = list.toArray(testargs);
 	}
 
 	public CommandContext(IMessage message, String name, String[] args) {
