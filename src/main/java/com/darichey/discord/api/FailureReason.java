23b643cd91a3b3592f37286b427662963d56f634
chrislo27 <kinchrislo@gmail.com>
2016-08-28 14:23:51 -0700

    Add documentation

--- src/main/java/com/darichey/discord/api/FailureReason.java
+++ src/main/java/com/darichey/discord/api/FailureReason.java
@@ -1,6 +1,15 @@
 package com.darichey.discord.api;
 
 public enum FailureReason {
+
+	/**
+	 * If the author of the command-invoker is missing the Discord permissions required by
+	 * {@link com.darichey.discord.api.Command.Options#requiredPermissions}
+	 */
 	AUTHOR_MISSING_PERMISSIONS,
+
+	/**
+	 * If the bot is missing Discord permissions (to send a message, etc.).
+	 */
 	BOT_MISSING_PERMISSIONS
 }
