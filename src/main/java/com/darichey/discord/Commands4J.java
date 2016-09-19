a36464ccfac264d7196006638f8086c7a9a3d49d
grandmind <darichey1@gmail.com>
2016-09-19 13:57:58 -0500

    Made C4J an IModule and bumped the version.

--- src/main/java/com/darichey/discord/Commands4J.java
+++ src/main/java/com/darichey/discord/Commands4J.java
@@ -1,15 +1,36 @@
 package com.darichey.discord;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import sx.blah.discord.Discord4J;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.modules.IModule;
 
-public class Commands4J {
-	public static final Logger LOGGER = (Discord4J.LOGGER instanceof Discord4J.Discord4JLogger ? new Commands4JLogger(Commands4J.class.getName()) : LoggerFactory.getLogger(Commands4J.class));
+public class Commands4J implements IModule {
+	@Override
+	public boolean enable(IDiscordClient iDiscordClient) {
+		return true;
+	}
 
-	public static class Commands4JLogger extends Discord4J.Discord4JLogger {
-		public Commands4JLogger(String name) {
-			super(name);
-		}
+	@Override
+	public void disable() {
+
+	}
+
+	@Override
+	public String getName() {
+		return "Commands4J";
+	}
+
+	@Override
+	public String getAuthor() {
+		return "Panda";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.1.0";
+	}
+
+	@Override
+	public String getMinimumDiscord4JVersion() {
+		return "2.6.0";
 	}
 }
